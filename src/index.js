793bcb2452ea2986e61b98a36f34c652301b0c3c
salatiel fiore <salatielfiore70@gmail.com>
2021-11-22 19:30:44 -0300

    versão 1.0

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './index.css';
+import Game from './Game';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+  
+  
+  
+  
